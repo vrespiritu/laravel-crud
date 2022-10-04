8ff4426023741d58a6ee21b26991ee920cb8be3e
24Vinz <rafaelazotillo@gmail.com>
2022-10-04 15:17:17 +0800

    3rd commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -55,7 +55,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return view('products.show', compact('product'));
     }
 
     /**
--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -2,10 +2,9 @@
 <html>
 <head>
     <title>Business</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootsrap/4.0.0-alpha/css/bootsrap.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-alpha1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<h1>Business</h1>
 <div class='container'>
     <br>
     @yield('content')
