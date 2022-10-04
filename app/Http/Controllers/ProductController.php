6d1778b1a31fcb039042b9fa377a91bd45045c38
24Vinz <rafaelazotillo@gmail.com>
2022-10-04 15:34:30 +0800

    Delete Commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -96,6 +96,8 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return redirect()->route('products.index')->with('success','Product deleted succesfully');
     }
 }
